311de02b9a1f44e614fae63d01d7c513f3494028
Adit Shetty <ashetty2308@gmail.com>
2021-07-23 11:21:13 -0400

    Added markers programmatically (originally manually done)

--- app/src/main/java/com/example/aireparaguay/MapFragment.java
+++ app/src/main/java/com/example/aireparaguay/MapFragment.java
@@ -18,6 +18,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -48,74 +49,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        new GetApiDataForMap().execute();
+
         return view;
     }
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-
-
-        //new GetApiDataForMap().execute();
-
-
-        Log.d("Regions", String.valueOf(longList));
-
         map = googleMap;
-        float zlevel = 11f;
-
-        LatLng Bo_Villa_Morra = new LatLng(-25.287982,  -57.58586);
-        map.addMarker(new MarkerOptions().position(Bo_Villa_Morra).title("Bo. Villa Morra (Asu)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Bo_Villa_Morra, zlevel));
-
-        LatLng Bo_Las_Mercedes = new LatLng(-25.291721,  -57.612337);
-        map.addMarker(new MarkerOptions().position(Bo_Las_Mercedes).title("Bo. Las Mercedes (Asu)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Bo_Las_Mercedes, zlevel));
-
-        LatLng Mercado_4 = new LatLng(-25.301672,   -57.628171);
-        map.addMarker(new MarkerOptions().position(Mercado_4).title("Mercado 4 (Asu)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Mercado_4, zlevel));
-
-
-        LatLng Bo_Obrero = new LatLng(-25.296891,  -57.646008);
-        map.addMarker(new MarkerOptions().position(Bo_Obrero).title("Bo. Obrero (Asu)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Bo_Obrero, zlevel));
-
-
-        LatLng Bo_Bella_Vista = new LatLng(-25.268886,  -57.591041);
-        map.addMarker(new MarkerOptions().position(Bo_Bella_Vista).title("Bo. Bella Vista (Asu)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Bo_Bella_Vista, zlevel));
-
-
-        LatLng Bo_Itá_Enramada = new LatLng(-25.348588,  -57.644467);
-        map.addMarker(new MarkerOptions().position(Bo_Itá_Enramada).title("Bo. Itá Enramada (Asu)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Bo_Itá_Enramada, zlevel));
-
-
-        LatLng Bo_Herrera = new LatLng(-25.295225,  -57.554957);
-        map.addMarker(new MarkerOptions().position(Bo_Herrera).title("Bo. Herrera (Asu)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Bo_Herrera, zlevel));
-
-        LatLng Zeballos_Cué = new LatLng(-25.230944,  -57.572913);
-        map.addMarker(new MarkerOptions().position(Zeballos_Cué).title("Zeballos Cué (Asu)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Zeballos_Cué, zlevel));
-
-        LatLng Bo_Policial = new LatLng(-25.2859963,  -57.5321415);
-        map.addMarker(new MarkerOptions().position(Bo_Policial).title("Bo. Policial (Luque)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Bo_Policial, zlevel));
-
-        LatLng Cuarto_Barrio = new LatLng( -25.271927,  -57.507497);
-        map.addMarker(new MarkerOptions().position(Cuarto_Barrio).title("Cuarto Barrio (Luque)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Cuarto_Barrio, zlevel));
-
-        LatLng Centro = new LatLng(-25.325833,  -57.54748);
-        map.addMarker(new MarkerOptions().position(Centro).title("Centro (Fdo. de la Mora)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Centro, zlevel));
-
-        LatLng Tres_Bocas = new LatLng(-25.355429,  -57.568617);
-        map.addMarker(new MarkerOptions().position(Tres_Bocas).title("Tres Bocas (Fdo. de la Mora)"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Tres_Bocas, zlevel));
-
-
-
     }
     public class GetApiDataForMap extends AsyncTask<Void, Void, Void> {
 
@@ -127,7 +68,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             regionList = new ArrayList<>();
             latList = new ArrayList<>();
             longList = new ArrayList<>();
-
 
             try {
                 URL url = new URL("https://aireparaguay.org/nodos_app");
@@ -150,7 +90,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                     regionList.add(apiData.getJSONObject(i).getString("region"));
                     latList.add(apiData.getJSONObject(i).getJSONObject("ubicacion").getDouble("lat"));
                     longList.add(apiData.getJSONObject(i).getJSONObject("ubicacion").getDouble("lng"));
-
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -160,7 +99,12 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
+            LatLng latLng;
+            for(int i = 0; i < regionList.size(); i++){
+                latLng = new LatLng(latList.get(i), longList.get(i));
+                map.addMarker(new MarkerOptions().position(latLng).title(regionList.get(i)));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,11f));
+            }
         }
     }
 }
